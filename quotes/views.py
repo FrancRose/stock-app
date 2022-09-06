c7dd25d4143f169a090fb411f7a85628bb1823dd
Francisco Rosete <fjrosete@gmail.com>
2022-09-06 13:25:19 -0700

    started api integration

--- quotes/views.py
+++ quotes/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html', {})
+    import requests
+    import json
+
+    #pk_7da063ed32a0479b93e5e2e9cd6d7619 API Key
+    api_call = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_7da063ed32a0479b93e5e2e9cd6d7619")
+
+    try:
+        api = json.loads(api_call.content)
+    except Exception as e:
+        api = "ERROR"
+
+    return render(request, 'index.html', {'api' : api})
 
 def about(request):
     return render(request, 'about.html', {})
