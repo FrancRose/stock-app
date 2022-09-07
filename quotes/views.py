502bf8edb7a999c88490f218198bd0a930e0ca39
Francisco Rosete <fjrosete@gmail.com>
2022-09-06 19:00:00 -0700

    add stock portfolio page and search

--- quotes/views.py
+++ quotes/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+# from contextlib import redirect_stderr
+# from email import message
+
+from django.shortcuts import render, redirect
+from .models import Stock
+from .forms import StockForm
+from django.contrib import messages
 
 def home(request):
     import requests
     import json
 
     if request.method == 'POST':
-        ticker = request.POST['ticker']
+        ticker = request.POST['ticker_symbol']
         api_call = requests.get("https://cloud.iexapis.com/stable/stock/" +  ticker + "/quote?token=pk_7da063ed32a0479b93e5e2e9cd6d7619")
 
         try:
@@ -22,3 +28,16 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def add_stock(request):
+    if request.method =='POST':
+        form = StockForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Stock Is Being Tracked"))
+            return redirect('add_stock')
+    else:
+        ticker = Stock.objects.all()
+        return render(request, 'add_stock.html', {'ticker': ticker})
+
