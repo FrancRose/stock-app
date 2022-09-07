ee7ca1d45a7d74caf37960f2a148cfd44e340535
Francisco Rosete <fjrosete@gmail.com>
2022-09-06 17:00:14 -0700

    linked api to search bar

--- quotes/views.py
+++ quotes/views.py
@@ -4,15 +4,21 @@ def home(request):
     import requests
     import json
 
-    #pk_7da063ed32a0479b93e5e2e9cd6d7619 API Key
-    api_call = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_7da063ed32a0479b93e5e2e9cd6d7619")
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_call = requests.get("https://cloud.iexapis.com/stable/stock/" +  ticker + "/quote?token=pk_7da063ed32a0479b93e5e2e9cd6d7619")
 
-    try:
-        api = json.loads(api_call.content)
-    except Exception as e:
-        api = "ERROR"
+        try:
+            api = json.loads(api_call.content)
+        except Exception as e:
+            api = "ERROR"
+        return render(request, 'index.html', {'api': api})
 
-    return render(request, 'index.html', {'api' : api})
+    else:
+        return render(request, 'index.html', {'ticker': "Enter a Ticker Symbol in the search box above"})
+
+
+    
 
 def about(request):
     return render(request, 'about.html', {})
