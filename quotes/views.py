c8b0e5b3cce2d1ad7bc0c7fdb0229e0a2c60f828
Francisco Rosete <fjrosete@gmail.com>
2022-09-08 11:14:39 -0700

    adding card & styling to index.html

--- quotes/views.py
+++ quotes/views.py
@@ -13,18 +13,20 @@ def home(request):
     if request.method == 'POST':
         ticker = request.POST['ticker_symbol']
         api_call = requests.get("https://cloud.iexapis.com/stable/stock/" +  ticker + "/quote?token=pk_7da063ed32a0479b93e5e2e9cd6d7619")
+        logo_call = requests.get("https://cloud.iexapis.com/stable/stock/" +  ticker + "/logo?token=pk_7da063ed32a0479b93e5e2e9cd6d7619")
 
         try:
             api = json.loads(api_call.content)
+            logo = json.loads(logo_call.content)
         except Exception as e:
-            api = "ERROR"
-        return render(request, 'index.html', {'api': api})
+            api = "API ERROR"
+            logo = "LOGO ERROR"
+        return render(request, 'index.html', {'api': api, 'logo': logo})
+
 
     else:
         return render(request, 'index.html', {'ticker': "Enter a Ticker Symbol in the search box above"})
 
-
-    
 
 def about(request):
     return render(request, 'about.html', {})
