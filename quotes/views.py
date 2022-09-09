24628668fb5f49a48bad30fb0598cf08eed28688
Francisco Rosete <fjrosete@gmail.com>
2022-09-09 11:27:46 -0700

    added news card to index.html

--- quotes/views.py
+++ quotes/views.py
@@ -14,14 +14,17 @@ def home(request):
         ticker = request.POST['ticker_symbol']
         api_call = requests.get("https://cloud.iexapis.com/stable/stock/" +  ticker + "/quote?token=pk_7da063ed32a0479b93e5e2e9cd6d7619")
         logo_call = requests.get("https://cloud.iexapis.com/stable/stock/" +  ticker + "/logo?token=pk_7da063ed32a0479b93e5e2e9cd6d7619")
+        news_call = requests.get("https://cloud.iexapis.com/stable/stock/" +  ticker + "/news?token=pk_7da063ed32a0479b93e5e2e9cd6d7619")
 
         try:
             api = json.loads(api_call.content)
             logo = json.loads(logo_call.content)
+            news = json.loads(news_call.content)
         except Exception as e:
             api = "API ERROR"
             logo = "LOGO ERROR"
-        return render(request, 'index.html', {'api': api, 'logo': logo})
+            news = "NEWS ERROR"
+        return render(request, 'index.html', {'api': api, 'logo': logo, 'news': news})
 
 
     else:
